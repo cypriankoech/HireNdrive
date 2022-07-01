4bf9529c444fb277ead7e3c2c19d5fc04a9f1736
Cyprian Koech <cypriankibet46@gmail.com>
2022-07-01 09:17:39 +0300

    more and more changes

---
+++ app/Models/Booking.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+    protected $table = 'bookings';
+    protected $primaryKey = 'bkid';
+
+
+
+    // protected $fillable =['model', 'RegNo', 'pricing', 'type', 'image'];
+}
---
+++ database/migrations/2022_07_01_055507_create_bookings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id("bkid");
+            $table->integer("clientID");
+            $table->integer("carID");
+            $table-> date("pkdate");
+            $table-> date("dpdate");
+            $table-> string("pkplace");
+            $table-> string("dpplace");
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            Schema::dropIfExists('bookings');
+        });
+    }
+};
